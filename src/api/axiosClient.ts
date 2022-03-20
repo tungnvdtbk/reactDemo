a20d1c9b5b474f04880c193844e37fd60d3c1f61
tungnv2 <tungnv@pixelz.com>
2022-03-20 14:05:41 +0700

    : clean code.

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -2,7 +2,7 @@
 import axios, { AxiosRequestConfig, AxiosResponse }  from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://js-post-api.herokuapp.com/api",
+    baseURL: "https://js-post-api.herokuapp.com/api",
     headers: {
         'Content-Type': 'application/json'    },
 });
